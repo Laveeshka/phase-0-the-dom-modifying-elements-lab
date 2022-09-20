f734e885f73ca8c9c8be30417ffb48e187f7e655
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-20 12:21:59 +1000

    commit tree 465fbdb3b2ea286f398517f69e14ff8a8a9d60f8

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ main.remove();
 
 // has a 'newHeader' variable that points to an <h1> node
 const newHeader = document.createElement("h1");
+newHeader.id ='victory';
+newHeader.textContent = "Lavee is the champion";
+
 document.body.append(newHeader);
 
 // the 'newHeader' variable that points to the <h1> node has an id of 'victory'
-newHeader.id ='victory';
 
 // the 'newHeader' variable that points to the <h1> node with an
 //id of 'victory' has the text "YOUR-NAME is the champion" 
 //(with your name in place of YOUR-NAME) inside it.
 
-newHeader.textContent = "Lavee is the champion";
